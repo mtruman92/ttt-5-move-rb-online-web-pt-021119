658adc9c0f5d352b2ccbb922107a33327c38adae
Mickae Truman <mtruman@nyit.edu>
2019-02-14 20:32:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index(user_input)
+  
 def move(board,index)
 board[index] = "X"
+return board
 end
 
 
