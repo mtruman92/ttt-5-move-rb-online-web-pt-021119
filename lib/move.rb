609f78785da6ca42ebdf66dd96c05ec8cda1434a
Mickae Truman <mtruman@nyit.edu>
2019-02-14 20:30:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,10 +1,3 @@
-def move(board,index)
-board[index] = "X"
-end
-def display_board(board)
-end
-expect(input_to_index(user_input)).to be_a(Integer)
-end
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -12,5 +5,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def input_to_index(user_input)
+def move(board,index)
+board[index] = "X"
+end
+
 
 # code your input_to_index and move method here!
