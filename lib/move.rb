76d21874109fc51d492496ec6fcfb81b82f3b0d1
Mickae Truman <mtruman@nyit.edu>
2019-02-14 20:37:10 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,14 +7,14 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input = user_input.to._i 
-  user_input -=1 
-  return user_input
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
 end
-  
-def move(board,index)
-board[index] = "X"
-return board
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
 end
 
 
